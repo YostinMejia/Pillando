ad525f40535bf0fdf1d702a8121349dc0bb06072
D3MB3 <110141509+YostinMejia@users.noreply.github.com>
2022-09-28 14:22:37 -0500

    count palabras adminLocal

--- restaurante.py
+++ restaurante.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as mtp
 from textblob import TextBlob
 from unicodedata import normalize
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from translate import Translator
 
 
 #Inicia coneccion con la bd
@@ -180,3 +181,58 @@ class Restaurante:
             self.comentarios=lista_comentarios
             for i in lista_comentarios:
                 print(i[0],f"Publicado el {i[1]}\n")
+
+
+    #Se mira también la palabra más repetida 
+    def mirarComentariosAdmin(self,palabra=False):
+
+        cur.execute("SELECT comentario,fecha FROM comentarios WHERE id_restaurante=?",(f"{self.id}",))
+        lista_comentarios=cur.fetchall()
+
+        repetidas={}
+        #Algunas de las palabras que no evalua el sentiemiento deseado
+        palabras_clave=["barato","caro","costoso","carísimo","baratisimo","valioso","económico","rebajado",
+        "increíble","volveremos","exquisita","exquisito","apretados","llena","lleno","recomendable","ricos","gustó"]
+        
+        if lista_comentarios==[]:
+            print("No hay comentarios")
+        else:
+
+            if palabra==False:
+                self.comentarios=lista_comentarios
+                repetidas={}
+                #Se miran todas las palabras y se guarda cuantas veces estan repetidas
+                for i in lista_comentarios:
+                    print(i[0],f"Publicado el {i[1]}\n")
+                    contando=TextBlob(i[0])
+                    contando=contando.word_counts
+                    llaves=list(contando.keys())
+                    for i in llaves:
+
+                        # Se evalua el sentimiento para guardar solamente las palbaras no neutrales 
+                        print(i,end=" ")
+                        palabra=Translator(from_lang="es", to_lang="en").translate(i) #Se traduce cada comentario
+                        print(palabra,end=" ")
+                        analisis=SentimentIntensityAnalyzer().polarity_scores(palabra) #Se analizan los sentimientos
+                        print(analisis["compound"]) #Se le asigna el valor arrojado por el analisis 
+
+                        #se mira que no sea una palabra neeutral o que este en palabras_claves
+                        if analisis["compound"] > 0.1 or analisis["compound"] < -0.1 or (i in palabras_clave): 
+
+                            if i in repetidas: #Se guardan las palabras 
+                                repetidas[i]+=contando[i]
+                            else:
+                                repetidas[i]=contando[i]
+                print(repetidas)
+            else:
+                repetidas={}
+                #Se miran todas las palabras y se guarda cuantas veces estan repetidas
+                for i in lista_comentarios:
+                    print(i[0])
+                    contando=TextBlob(i[0])
+                    contando=contando.word_counts[palabra]
+                    print(contando)
+                    # repetidas[i]=contando[i]
+                # print(repetidas)
+
+                
--- usuario.py
+++ usuario.py
@@ -1,6 +1,5 @@
 from datetime import date
 import sqlite3 as sql
-from traceback import print_tb
 
 from restaurante import *
 
@@ -46,92 +45,3 @@ class Usuario:
         obj_temp.mirarProductos(producto)
   
 
-class Administrador():
-    def __init__(self,id:str,nombre:str) -> None:
-        self.id=id
-        self.nombre=nombre
-
-    def agregarRestaurante(self,nombre:str,ubicacion:str,horario:str)-> None:
-
-        #qmark style 
-        restaurante=(None,nombre,ubicacion,0,horario,)
-        cur.execute("INSERT INTO restaurante VALUES(?,?,?,?,?)",restaurante)
-        print("Restaurante ingresado")
-        con.commit()
-
-
-    def eliminarComentarios(self,comentario:str)-> None:
-        cur.execute("DELETE FROM comentarios WHERE id= ?",(f"{comentario}",))
-        con.commit()
-        print("Comentario eliminado")
-        # cur.execute("SELECT * FROM comentarios WHERE id=?",(f"{id_comentario}",))
-        # dato=cur.fetchall()
-        # print(dato)
-        
-class AdminLocal:
-
-    def __init__(self,id_restaurante:str) -> None:
-        self.id_restaurante:str=id_restaurante
-
-    def agregarProducto(self,prod_nombre:str ,prod_precio:int ,prod_descripcion:str ,prod_alias:str)-> None:
-
-        producto=(None,prod_nombre,self.id_restaurante,prod_precio,prod_descripcion,prod_alias,)
-        
-        cur.execute("INSERT INTO productos VALUES(?,?,?,?,?,?)",producto)
-        con.commit()
-        print("Producto Agregado")
-        
-    
-    def actualizarProductos(self,id:str,dato:str,cambio:str)-> None:
-        
-        cur.execute("SELECT * FROM productos WHERE id=? ",(f"{id}",))
-        producto=cur.fetchall()
-
-        nombre=producto[0][1] #1
-        precio=producto[0][3] #2
-        descripcion=producto[0][4] #3
-        alias=producto[0][5] #4
-        
-        if dato!="eliminar":
-
-            if dato=="nombre":
-                nombre=limpiarStr(nombre)
-                nombre=cambio
-            elif dato=="precio":
-                precio=int(cambio)
-            elif dato=="descripcion":
-                descripcion=cambio
-            elif dato=="alias":
-                alias=cambio
-            
-            else:
-                cambio=input("Digite un cambio valido: ")
-                self.actualizarProductos(id,cambio)
-
-            #Se aplica el cambio seleccionado
-            actualizacion=(nombre,precio,descripcion,alias,id,)
-            cur.execute("UPDATE productos SET nombre=? , precio=?, descripcion=?, alias=? WHERE id=? ",actualizacion)
-            con.commit()
-            print("producto actualizado")
-        
-        #Eliminar producto por id
-        else:
-            cur.execute("DELETE FROM productos WHERE id=? ",(f"{id}"),)
-            con.commit()
-            print("Producto eliminado")
-        
-    def cambiarInfoRestaurante(self,dato:str,cambio:str):
-
-        if dato=="nombre":
-            cur.execute("UPDATE restaurante SET nombre=? WHERE id=?",(f"{cambio}",f"{self.id_restaurante}",))
-            con.commit()
-        elif dato=="ubicacion":
-            cur.execute("UPDATE restaurante SET ubicacion=? WHERE id=?",(f"{cambio}",f"{self.id_restaurante}",))
-            con.commit()
-        elif dato=="horario":
-            cur.execute("UPDATE restaurante SET horario=? WHERE id=?",(f"{cambio}",f"{self.id_restaurante}",))
-            con.commit()
-    
-    def mirarCalificacion(self):
-        obj_temp=Restaurante(self.id_restaurante)
-        obj_temp.mirarCalificacion()
